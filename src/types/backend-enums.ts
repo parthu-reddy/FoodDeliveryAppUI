2937fca7f54fe7ebb2c7f9479b65db0315fd5c8b
Parthu <parthudhanireddy@gmail.com>
2026-08-19 05:30:18 +0530

    chore: Contract testing phase 4 completion and Context load stabilization

--- src/types/backend-enums.ts
+++ src/types/backend-enums.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2026-08-17 20:32:09.
+// Generated using typescript-generator version 3.2.1263 on 2026-08-18 23:03:54.
 
 export enum AccountType {
     CUSTOMER = "CUSTOMER",
