a7167a3b99c3aae8a8b1bb5b45cfa0f9b7bce61d
Parthu <parthudhanireddy@gmail.com>
2026-08-20 20:24:36 +0530

    chore: Ad Platform review updates and remediation fixes

--- src/types/backend-enums.ts
+++ src/types/backend-enums.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2026-08-19 14:54:38.
+// Generated using typescript-generator version 3.2.1263 on 2026-08-20 20:15:13.
 
 export enum AccountType {
     CUSTOMER = "CUSTOMER",
