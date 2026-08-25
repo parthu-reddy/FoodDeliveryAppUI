1f693b45f04c52d8113a2f8530438b14e1f9f0c8
Parthu <parthudhanireddy@gmail.com>
2026-08-25 16:05:26 +0530

    chore: update backend-enums timestamp

--- src/types/backend-enums.ts
+++ src/types/backend-enums.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2026-08-25 12:23:25.
+// Generated using typescript-generator version 3.2.1263 on 2026-08-25 13:45:15.
 
 export enum AccountType {
     CUSTOMER = "CUSTOMER",
