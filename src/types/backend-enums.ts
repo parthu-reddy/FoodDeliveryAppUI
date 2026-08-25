fe231daf9d9ba2878eae90fdda2c7857470a2f83
Parthu <parthudhanireddy@gmail.com>
2026-08-25 18:37:28 +0530

    fix: added specific backend enum to UI

--- src/types/backend-enums.ts
+++ src/types/backend-enums.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2026-08-25 13:45:15.
+// Generated using typescript-generator version 3.2.1263 on 2026-08-25 18:29:04.
 
 export enum AccountType {
     CUSTOMER = "CUSTOMER",
